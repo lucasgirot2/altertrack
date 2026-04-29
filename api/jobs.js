fea57fc7a70d83831c0d09722e07fd394e0c498a
Lucas <lucasgirot2@gmail.com>
2026-04-29 19:08:03 +0200

    fix search endpoint

--- api/jobs.js
+++ api/jobs.js
@@ -5,8 +5,8 @@ module.exports = async function handler(req, res) {
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const CLIENT_ID = 'PAR_altertrack_aa4922c1549db67b92b4305c82fac9e804ccbb79908fa054abc9b0f529d1c995';
-  const CLIENT_SECRET = 'd779d51244a1725524e56b6c531d52930b9f613735758317912e02fafba5b376';
+  const CLIENT_ID = 'TON_CLIENT_ID';
+  const CLIENT_SECRET = 'TA_CLE_SECRETE';
   const rad = req.query.radius || '20';
 
   try {
@@ -14,30 +14,25 @@ module.exports = async function handler(req, res) {
     params.append('grant_type', 'client_credentials');
     params.append('client_id', CLIENT_ID);
     params.append('client_secret', CLIENT_SECRET);
-    params.append('scope', 'api_offresdemploiv2 application_PAR_altertrack_aa4922c1549db67b92b4305c82fac9e804ccbb79908fa054abc9b0f529d1c995');
+    params.append('scope', 'api_offresdemploiv2');
 
     const tokenRes = await fetch(
       'https://entreprise.francetravail.fr/connexion/oauth2/access_token?realm=%2Fpartenaire',
       { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: params }
     );
 
-    if (!tokenRes.ok) {
-      const txt = await tokenRes.text();
-      throw new Error(`Token ${tokenRes.status}: ${txt}`);
-    }
-
+    if (!tokenRes.ok) throw new Error(`Token ${tokenRes.status}`);
     const { access_token } = await tokenRes.json();
 
-    const searchUrl = new URL('https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search');
-    searchUrl.searchParams.set('motsCles', 'chef de projet digital alternance');
-    searchUrl.searchParams.set('commune', '95088');
-    searchUrl.searchParams.set('rayon', rad);
-    searchUrl.searchParams.set('typeContrat', 'CIE,CA');
-    searchUrl.searchParams.set('range', '0-49');
-
-    const searchRes = await fetch(searchUrl.toString(), {
-      headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${access_token}` }
-    });
+    const searchRes = await fetch(
+      `https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search?motsCles=chef+de+projet+digital&commune=95088&rayon=${rad}&typeContrat=CIE,CA&range=0-49`,
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${access_token}`
+        }
+      }
+    );
 
     if (!searchRes.ok) {
       const txt = await searchRes.text();
